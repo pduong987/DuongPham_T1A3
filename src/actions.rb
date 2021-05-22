f8aa802dc9d4b54eada3f677fa151065c0599018
Duong Pham <ptduong987@gmail.com>
2021-05-23 00:09:12 +1000

    get the mortgage calculatior working

--- src/actions.rb
+++ src/actions.rb
@@ -13,3 +13,33 @@ def transfer
         puts "sorry wrong input"
     end
 end
+
+def mortgage_calculator
+    #Get information from client
+    puts "Hi, Welcome to Mortgage calculator at Bank smart."
+    puts "Please answer a few questions below"
+    puts "1.How much your total household income ?\n\n"
+    income = gets.chomp.to_i
+    puts "2.How many dependants children do you have?\n\n"
+    dependants = gets.chomp.to_i
+
+    puts "3.Purchase type: Invesment or Home owner?\n\n"
+    purchase_type= gets.chomp
+    
+    #Calculating borrow capacity
+    borrowing_capacity = (income * 5) - (100000 * dependants)
+    #setting default interest rate to connect to the loop 
+    interest_rate = 0.0
+    #loop what the house purchase type for inversment or home
+    if purchase_type == "home"
+        interest_rate = 2.0
+    elsif purchase_type == "investment"
+        interest_rate = 4.0
+    else   
+        # WRONG SHOW ERROR
+        puts "Sorry, please input 'home' or 'investment' only"
+    end
+    
+    puts "Thank you for all information input. Your borrowing capacity is #{borrowing_capacity} with #{interest_rate}%."
+    
+end
--- src/index.rb
+++ src/index.rb
@@ -17,6 +17,7 @@ user = database.getUserByUsername(username)
 #Loop to check if the user name is exits, will return user name with gretting,
 if user != nil
     puts "Hi #{user.getName} " 
+    puts 
     #new variable userId by get Id out of the user
     userId = user.getId
     #pass the user information to new array loggedInUserAccounts
@@ -41,6 +42,7 @@ if user != nil
     end
     puts ""
     accOpt = gets.chomp
+    puts 
     #new account variable will be equal to the account option
     account = loggedInUserAccounts[accOpt.to_i]
 
@@ -51,35 +53,39 @@ if user != nil
         puts "============================="
         puts "What would you like to do with this account"
         puts "============================="
-        puts "||      1- Deposite        ||"
-        puts "||      2- Withdraw        ||"
-        puts "||      3- Balance         ||" 
-        puts "||      4- Transfer        || "
-        puts "||      5- Mortgage        ||"
-        puts "||      6- Entertainment   ||"
+        puts "||    1- Deposite          ||"
+        puts "||    2- Withdraw          ||"
+        puts "||    3- Balance           ||" 
+        puts "||    4- Transfer          || "
+        puts "||    5- Mortgage_cal      ||"
+        puts "||    6- Entertainment     ||"
         puts "============================="
         puts "============================="
         puts
         puts
 
         action = gets.chomp
+        puts
         #Loop through to get each option out from the action input
         if action == "1"
             puts "Please enter the amount: "
-            amount = gets.chomp
+            amount = gets.chomps
+            #Get deposite been calculated in deposite method and return the amount 
             account.deposite(amount.to_i)
             puts "Thank you, we deposited $#{amount} to account #{account.getName}, your new balance is #{account.getBalance}"
         elsif action == "2"
             puts "Please enter the amount:"
             amount = gets.chomp
-              account.deposite(amount.to_i)
-            puts "Thank you, we deposited $#{amount} to account #{account.getName}, your new balance is #{account.getBalance}"
+            #Get withdraw been calculated in withdraw method and return the amount
+            account.withdraw(amount.to_i)
+            puts "Thank you, we have withdrawn $#{amount} from account #{account.getName}, your new balance is #{account.getBalance}"
         elsif action == "3"
             puts "Your balance for #{account.getName} is #{account.getBalance}"
         elsif action == "4"
+            # Get result from the loop in transfer method and return the answer
            transfer
         elsif action == "5"
-            puts "Have you got a mortgage account with us?"
+            mortgage_calculator
         elsif action == "6"
             puts "Hope you have enjoy our recommendation. "
         else
