2677c8601f959c085d59c2e2d75e219914f1a6f2
Duong Pham <ptduong987@gmail.com>
2021-05-23 21:23:40 +1000

    Adding more logic

--- src/index.rb
+++ src/index.rb
@@ -21,140 +21,187 @@ def display_ascii_art
     return File.read("art.txt")
   end
 
-  puts display_ascii_art.red
-
-#   puts ColorizedString["This is blue"].colorize(:blue)
-
 #Creat a new variable database
-database = Database.new
+database = Database.new  
 
-#Login to the bank, get user input
-puts "Login: "
-puts
-username = gets.chomp
-puts 
-puts
+show_menu_1 = true
+show_menu_2 = true
+show_menu_3 = true
+show_menu_4 = true
 
-#Get the user for the specific user name
-user = database.getUserByUsername(username)
+# Menu 1 = The Main login menu
 
-#Loop to check if the user name is exits, will return user name with gretting,
-if user != nil
-    puts "Hi #{user.getName}. Welcome to Bank Smart." 
-    puts 
-    #new variable userId by get Id out of the user
-    userId = user.getId
-    #pass the user information to new array loggedInUserAccounts
-    loggedInUserAccounts = database.getAccountsByUserId(userId)
+# While the user wants to see Menu 1
+while show_menu_1 == true
 
-    account = nil
+    # Show the welcome banner
+    puts display_ascii_art.red
 
-    #if the length of loggedInuserAccounts greater than 0
-    if loggedInUserAccounts.length > 0
 
-        account_options = []
+    while show_menu_2 == true
 
-        loggedInUserAccounts.each { |acc|
-            account_options.push(acc.getName)
-        }
+        #Login to the bank, get user input
+        puts "Login: "
+        username = gets.chomp
 
-        account_options.push("Exit")
+        #Get the user for the specific user name
+        user = database.getUserByUsername(username)
 
-        selected_account_name = prompt.select("Please select one of your accounts:",account_options)
+        #Loop to check if the user name is exits, will return user name with gretting,
+        if user != nil
 
-        if selected_account_name == "Exit"
-            exit
-        end
-
+            # Welcome the selected user
+            puts "Hi #{user.getName}. Welcome to Bank Smart." 
  
-        # Now we have the name of the account, lets find the object
-        loggedInUserAccounts.each { |acc|
+            #new variable userId by get Id out of the user
+            userId = user.getId
 
-            if acc.getName == selected_account_name
-                account = acc
-             
+            # Get all accounts for the selected user
+            loggedInUserAccounts = database.getAccountsByUserId(userId)
+
+      
+            #if the length of loggedInuserAccounts greater than 0
+            if loggedInUserAccounts.length > 0
+
+                # Holds a list of account options based on selected user
+                account_options = []
+
+                # Loop through and add each account as an option
+                loggedInUserAccounts.each { |acc|
+                    account_options.push(acc.getName)
+                }
+
+                # Add a final option which allows the user to exist (break loop)
+                account_options.push("Exit")
+
+                while show_menu_3 == true
+
+                    # This is the account a user then selects
+                    account = nil
+
+                    selected_account_name = prompt.select("Please select one of your accounts:",account_options)
+
+                    # If the user has broken the while loop, we stop asking them to select an account and got back to main screen
+                    if selected_account_name == "Exit"
+                        break
+                    end
+
+                    # Now we have the name of the account, lets find the object
+                    loggedInUserAccounts.each { |acc|
+
+                        if acc.getName == selected_account_name
+                            account = acc
+                        end
+
+                    }
+
+                    #loop to see if the account exits
+                    if account != nil
+
+                        #Using gem TTY-promt to slect the coices.
+                        choices = ["Deposite", "Withdraw", "Balance", "Transfer", "Mortgage Calculator", "Entertainment", "Exit"]
+
+
+                        # Show menu for selecting what to do with account
+                        while show_menu_4 == true
+
+                            action = prompt.select("What would you like to do with this account?", choices)
+
+                            #Loop through to get each option out from the action input
+                            if action == "Deposite"
+                                
+                                puts "Please enter the amount: "
+                                amount = gets.chomp
+                                #Get deposite been calculated in deposite method and return the amount 
+                                account.deposite(amount.to_i)
+                                puts "Thank you, we deposited $#{amount} to your #{account.getName}, your new balance is $#{account.getBalance}"
+
+                            elsif action == "Withdraw"
+                                
+                                puts "Please enter the amount:"
+                                amount = gets.chomp
+                                #Get withdraw been calculated in withdraw method and return the amount
+                                account.withdraw(amount.to_i)
+                                puts "Thank you, we have withdrawn $#{amount} from your #{account.getName}, your new balance is $#{account.getBalance}"
+
+                            elsif action == "Balance"
+
+                                puts "Your balance for #{account.getName} is $#{account.getBalance}"
+                            
+                            elsif action == "Transfer"
+                                
+                                # Get result from the loop in transfer method and return the answer
+                                transfer
+
+                            elsif action == "Mortgage Calculator"
+
+                                #Get mortgage calculator from action. 
+                                mortgage_calculator
+                            
+                            elsif action == "Entertainment"
+
+                                # Ask the user where they want to go. Usee the query directly on the Google Places API
+                                puts "Let's have some fun... what are you looking for ?"
+                                query = gets.chomp
+
+                                # Call the Google Places API with query we got from user
+                                places = gmc.spots_by_query(query)
+
+                                puts ""
+
+                                available_places = []
+
+                                # Make a simple array of places with the name and address
+                                places.each {|place|
+                                    available_places.push("#{place.name} at #{place.formatted_address}")
+                                }
+
+                                # Ask the user to select one of those places...
+                                selected_place = prompt.select("I found these places, where do you want to go...", available_places)
+
+                                # Simple message to say we are going to that place... could be much more in the future
+                                puts "Thanks... we will go to " + selected_place
+
+                            elsif action == "Exit"
+                                puts "Thanks for using Banks Smart. Good bye!"
+                                break
+                              
+                            else
+                                puts "Sorry thats not a valid action!"
+                            end
+
+                            # Finally, ask if there is anything else to do
+
+                            action = prompt.select("Would you like to do anything else ?", ["Yes","No"])
+
+                            if action != "Yes"
+                                puts "Thank you for using Bank Smart, have a great day!"
+                                show_menu_1 = true
+                                show_menu_2 = true
+                                show_menu_3 = false
+                                show_menu_4 = false
+                            end
+                        end
+                    else
+                        puts "Sorry, that is not a valid account!"
+                        break
+                    end
+                    
+
+                end
+
+            #if length of the array no greater than 0, then below msg will print out
+            else
+                puts "Sorry you dont have any accounts!"
+                break
             end
 
-        }
-
-    #if length of the array no greater than 0, then below msg will print out
-    else
-        puts "Sorry you dont have any accounts!"
-    end
-    puts ""
-    puts 
-
-    #loop to see if the account exits
-    if account != nil
-
-        #Using gem TTY-promt to slect the coices.
-        choices = ["Deposite", "Withdraw", "Balance", "Transfer", "Mortgage Calculator", "Entertainment", "Exit"]
-        action = prompt.select("What would you like to do with this account?", choices)
-
-        #Loop through to get each option out from the action input
-        if action == "Deposite"
-            puts "Please enter the amount: "
-            amount = gets.chomp
-            #Get deposite been calculated in deposite method and return the amount 
-            account.deposite(amount.to_i)
-            puts "Thank you, we deposited $#{amount} to your #{account.getName}, your new balance is $#{account.getBalance}"
-            puts "Would you like to do anythingesle?"
-
-        elsif action == "Withdraw"
-            puts "Please enter the amount:"
-            amount = gets.chomp
-            #Get withdraw been calculated in withdraw method and return the amount
-            account.withdraw(amount.to_i)
-            puts "Thank you, we have withdrawn $#{amount} from your #{account.getName}, your new balance is $#{account.getBalance}"
-        elsif action == "Balance"
-            puts "Your balance for #{account.getName} is #{account.getBalance}"
-        elsif action == "Transfer"
-            # Get result from the loop in transfer method and return the answer
-           transfer
-        elsif action == "Mortgage Calculator"
-            #Get mortgage calculator from action. 
-            mortgage_calculator
-        elsif action == "Entertainment"
-
-            # Ask the user where they want to go... we will use the query directly on the Google Places API
-            puts "Let's have some fun... what are you looking for ?"
-            query = gets.chomp
-
-            # Call the Google Places API with query we got from use
-            places = gmc.spots_by_query(query)
-
-            puts ""
-
-            available_places = []
-
-            # Make a simple array of places with the name and address
-            places.each {|place|
-                available_places.push("#{place.name} at #{place.formatted_address}")
-            }
-
-            # Ask the user to select one of those places...
-            selected_place = prompt.select("I found these places, where do you want to go...", available_places)
-
-            # Simple message to say we are going to that place... could be much more in the future
-            puts "Thanks... we will go to " + selected_place
-            
-        elsif action == "Exit"
-            puts "Thanks for using Banks Smart. Good bye!"
-            exit
+        #User name not exits , then print wrong input. 
         else
-            puts "Sorry thats not a valid action!"
+            puts "Sorry #{username}, wrong input! " 
         end
-
-    else
-        puts "Sorry, that is not a valid account!"
     end
+
+
     
-
-
-#User name not exits , then print wrong input. 
-else
-    puts "Sorry #{username}, wrong input! " 
 end
-
-
-
